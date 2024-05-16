e44cfc2c3f777c9f31cc543392f1ef82a6ee06dd
sj611.yang <sj611.yang@samsung.com>
2024-05-16 16:51:05 +0900

    [feature] page 37

--- Sample-Test1/prime-factors_test.cpp
+++ Sample-Test1/prime-factors_test.cpp
@@ -1,11 +1,16 @@
 #include "pch.h"
 #include "../PrimeFactor/prime-factors.cpp"
 #include <vector>
-#include "prime-factors_test.h"
 using namespace std;
 
 TEST(PrimeFactors, Of1) {
 	PrimeFactor prime_factor;
 	vector<int> expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
+}
+
+TEST(PrimeFactors, Of2) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
 }
